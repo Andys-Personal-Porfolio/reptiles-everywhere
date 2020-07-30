bef514698fb747b58f8320b347db9bab007f3306
Andy Tom <andrew.tom@colorado.edu>
2020-07-29 21:58:38 -0600

    Adds styling to make books into rows

--- src/Components/Inventory/Inventory.js
+++ src/Components/Inventory/Inventory.js
@@ -7,7 +7,7 @@ const Inventory = ({ childrensBooks }) => {
     // console.log(childrensBooks.map(book => book))
     return childrensBooks.map((book, i) => {
       return (
-        <div key={i} className="book-container">
+        <div key={i} className="book-info">
           <h2>{book.title}</h2>
           <img src={`http://covers.openlibrary.org/b/ID/${book.cover_id}-M.jpg`} alt={book.title + " cover"} />
         </div>
@@ -17,7 +17,9 @@ const Inventory = ({ childrensBooks }) => {
 
   return (
     <>
-    { childrensBooks.length && createBookElement()}
+    <div className="book-container" >
+      { childrensBooks.length && createBookElement()}
+    </div>
     </>
   )
 
