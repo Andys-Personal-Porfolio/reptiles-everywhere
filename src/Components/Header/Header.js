a4c3eea30f0dcc5a126c6a7c1d0acafdea732ac0
Andy Tom <andrew.tom@colorado.edu>
2020-07-31 22:04:26 -0600

    Adds radio buttons for categories

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,6 +7,16 @@ const Header = ({searchBooks}) => {
       <h2>Reptiles Everywhere!</h2>
       <p> Reptiles are turtles, snakes, lizards, alligators and crocodiles. </p>
       <input type="text" onChange={(event) => searchBooks(event)}/>
+      <input type="radio" name="search-criteria" id="snakes"/>
+      <label for="snakes">Snakes</label>
+      <input type="radio" name="search-criteria" id="lizards"/>
+      <label for="lizards">Lizards</label>
+      <input type="radio" name="search-criteria" id="alligators"/>
+      <label for="alligators">Alligators</label>
+      <input type="radio" name="search-criteria" id="crocodiles"/>
+      <label for="crocodiles">Crocodiles</label>
+      <input type="radio" name="search-criteria" id="turtles"/>
+      <label for="turtles">Turtles</label>
     </header>
   )
 }
