09b99948e9edef043719819892f443c3872cf7f5
Andy Tom <andrew.tom@colorado.edu>
2020-07-31 18:57:34 -0600

    Sets up environment for mock testing, adds test for rendering mock book covers

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, waitFor, getAllByRole } from '@testing-library/react'
+import App from './App'
+import { MemoryRouter } from 'react-router-dom';
+import { fetchBooks } from '../../ApiCalls'
+import '@testing-library/jest-dom';
+import fetchBooksMockData  from './fetchBooksMockData'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+jest.mock("../../ApiCalls");
+
+
+describe('App', () => {
+
+  fetchBooks.mockResolvedValue(fetchBooksMockData)
+  
+  it('should render title on load', async () => {
+    const { getByRole } = render(<MemoryRouter><App /></MemoryRouter>)
+    const title = await waitFor(() => getByRole('heading', 'Reptiles Everywhere!'))
+    expect(title).toBeInTheDocument()
+  })
+
+  it('should render covers of books on load', async () => {
+    const { getByRole, getAllByRole} = render(<MemoryRouter><App/></MemoryRouter>)
+    const coverImg = await waitFor(() => getByRole('img', { name: "Smart Kids: Reptiles and Amphibians cover"}))
+    const covers = await waitFor(() => getAllByRole('img'))
+
+    expect(covers.length).toEqual(2)
+    expect(coverImg).toBeInTheDocument()
+
+  })
+
+})
---
+++ src/Components/App/fetchBooksMockData.js
@@ -0,0 +1,203 @@
+const fetchBooksMockData = {
+  "kind": "books#volumes",
+  "totalItems": 1881,
+  "items": [
+    {
+      "kind": "books#volume",
+      "id": "TtA4DwAAQBAJ",
+      "etag": "vrnw0LOS8gw",
+      "selfLink": "https://www.googleapis.com/books/v1/volumes/TtA4DwAAQBAJ",
+      "volumeInfo": {
+        "title": "All About the Reptiles of the World - Animal Books | Children's Animal Books",
+        "authors": [
+          "Baby Professor"
+        ],
+        "publisher": "Speedy Publishing LLC",
+        "publishedDate": "2017-06-15",
+        "description": "Let’s talk about the reptiles. What kind of animals are they? What are the features that make them unique from all the other animals? What are the most popular reptiles in the world? All these questions will be asked when you read a copy of this educational animal book for children. Improve your child’s knowledge of the outside world. Encourage him/her to read today!",
+        "industryIdentifiers": [
+          {
+            "type": "ISBN_13",
+            "identifier": "9781541939608"
+          },
+          {
+            "type": "ISBN_10",
+            "identifier": "1541939603"
+          }
+        ],
+        "readingModes": {
+          "text": true,
+          "image": true
+        },
+        "pageCount": 64,
+        "printType": "BOOK",
+        "categories": [
+          "Young Adult Nonfiction"
+        ],
+        "maturityRating": "NOT_MATURE",
+        "allowAnonLogging": false,
+        "contentVersion": "1.1.1.0.preview.3",
+        "panelizationSummary": {
+          "containsEpubBubbles": false,
+          "containsImageBubbles": false
+        },
+        "imageLinks": {
+          "smallThumbnail": "http://books.google.com/books/content?id=TtA4DwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+          "thumbnail": "http://books.google.com/books/content?id=TtA4DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        },
+        "language": "en",
+        "previewLink": "http://books.google.com/books?id=TtA4DwAAQBAJ&printsec=frontcover&dq=reptiles+children+books&hl=&as_brr=3&as_pt=BOOKS&cd=1&source=gbs_api",
+        "infoLink": "https://play.google.com/store/books/details?id=TtA4DwAAQBAJ&source=gbs_api",
+        "canonicalVolumeLink": "https://play.google.com/store/books/details?id=TtA4DwAAQBAJ"
+      },
+      "saleInfo": {
+        "country": "US",
+        "saleability": "FOR_SALE",
+        "isEbook": true,
+        "listPrice": {
+          "amount": 2.99,
+          "currencyCode": "USD"
+        },
+        "retailPrice": {
+          "amount": 2.99,
+          "currencyCode": "USD"
+        },
+        "buyLink": "https://play.google.com/store/books/details?id=TtA4DwAAQBAJ&rdid=book-TtA4DwAAQBAJ&rdot=1&source=gbs_api",
+        "offers": [
+          {
+            "finskyOfferType": 1,
+            "listPrice": {
+              "amountInMicros": 2990000,
+              "currencyCode": "USD"
+            },
+            "retailPrice": {
+              "amountInMicros": 2990000,
+              "currencyCode": "USD"
+            },
+            "giftable": true
+          }
+        ]
+      },
+      "accessInfo": {
+        "country": "US",
+        "viewability": "PARTIAL",
+        "embeddable": true,
+        "publicDomain": false,
+        "textToSpeechPermission": "ALLOWED",
+        "epub": {
+          "isAvailable": true,
+          "acsTokenLink": "http://books.google.com/books/download/All_About_the_Reptiles_of_the_World_Anim-sample-epub.acsm?id=TtA4DwAAQBAJ&format=epub&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"
+        },
+        "pdf": {
+          "isAvailable": true,
+          "acsTokenLink": "http://books.google.com/books/download/All_About_the_Reptiles_of_the_World_Anim-sample-pdf.acsm?id=TtA4DwAAQBAJ&format=pdf&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"
+        },
+        "webReaderLink": "http://play.google.com/books/reader?id=TtA4DwAAQBAJ&hl=&as_brr=3&as_pt=BOOKS&printsec=frontcover&source=gbs_api",
+        "accessViewStatus": "SAMPLE",
+        "quoteSharingAllowed": false
+      },
+      "searchInfo": {
+        "textSnippet": "What are the features that make them unique from all the other animals? What are the most popular reptiles in the world? All these questions will be asked when you read a copy of this educational animal book for children."
+      }
+    },
+    {
+      "kind": "books#volume",
+      "id": "9j1lDwAAQBAJ",
+      "etag": "byrMjvRPHIg",
+      "selfLink": "https://www.googleapis.com/books/v1/volumes/9j1lDwAAQBAJ",
+      "volumeInfo": {
+        "title": "Smart Kids: Reptiles and Amphibians",
+        "authors": [
+          "Roger Priddy"
+        ],
+        "publisher": "Priddy Books",
+        "publishedDate": "2018-08-14",
+        "description": "From the poisonous rattlesnake to the color-changing chameleon Roger Priddy’s Smart Kids Reptiles and Amphibians is an amazing introduction into the world of these fascinating creatures. Ideal for helping children in elementary or first grade with school projects, this reference book is filled with full-color photographs and incredible facts about each animal, such as how big each creature is and their danger rating. With over 30 stickers just for fun, this paperback book is a must-have for kids who are interested in the world around them.",
+        "industryIdentifiers": [
+          {
+            "type": "ISBN_13",
+            "identifier": "9781684490196"
+          },
+          {
+            "type": "ISBN_10",
+            "identifier": "1684490197"
+          }
+        ],
+        "readingModes": {
+          "text": true,
+          "image": true
+        },
+        "pageCount": 32,
+        "printType": "BOOK",
+        "categories": [
+          "Juvenile Nonfiction"
+        ],
+        "maturityRating": "NOT_MATURE",
+        "allowAnonLogging": false,
+        "contentVersion": "1.1.1.0.preview.3",
+        "panelizationSummary": {
+          "containsEpubBubbles": false,
+          "containsImageBubbles": false
+        },
+        "imageLinks": {
+          "smallThumbnail": "http://books.google.com/books/content?id=9j1lDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+          "thumbnail": "http://books.google.com/books/content?id=9j1lDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        },
+        "language": "en",
+        "previewLink": "http://books.google.com/books?id=9j1lDwAAQBAJ&printsec=frontcover&dq=reptiles+children+books&hl=&as_brr=3&as_pt=BOOKS&cd=2&source=gbs_api",
+        "infoLink": "https://play.google.com/store/books/details?id=9j1lDwAAQBAJ&source=gbs_api",
+        "canonicalVolumeLink": "https://play.google.com/store/books/details?id=9j1lDwAAQBAJ"
+      },
+      "saleInfo": {
+        "country": "US",
+        "saleability": "FOR_SALE",
+        "isEbook": true,
+        "listPrice": {
+          "amount": 9.99,
+          "currencyCode": "USD"
+        },
+        "retailPrice": {
+          "amount": 9.99,
+          "currencyCode": "USD"
+        },
+        "buyLink": "https://play.google.com/store/books/details?id=9j1lDwAAQBAJ&rdid=book-9j1lDwAAQBAJ&rdot=1&source=gbs_api",
+        "offers": [
+          {
+            "finskyOfferType": 1,
+            "listPrice": {
+              "amountInMicros": 9990000,
+              "currencyCode": "USD"
+            },
+            "retailPrice": {
+              "amountInMicros": 9990000,
+              "currencyCode": "USD"
+            },
+            "giftable": true
+          }
+        ]
+      },
+      "accessInfo": {
+        "country": "US",
+        "viewability": "PARTIAL",
+        "embeddable": true,
+        "publicDomain": false,
+        "textToSpeechPermission": "ALLOWED",
+        "epub": {
+          "isAvailable": true,
+          "acsTokenLink": "http://books.google.com/books/download/Smart_Kids_Reptiles_and_Amphibians-sample-epub.acsm?id=9j1lDwAAQBAJ&format=epub&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"
+        },
+        "pdf": {
+          "isAvailable": true,
+          "acsTokenLink": "http://books.google.com/books/download/Smart_Kids_Reptiles_and_Amphibians-sample-pdf.acsm?id=9j1lDwAAQBAJ&format=pdf&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"
+        },
+        "webReaderLink": "http://play.google.com/books/reader?id=9j1lDwAAQBAJ&hl=&as_brr=3&as_pt=BOOKS&printsec=frontcover&source=gbs_api",
+        "accessViewStatus": "SAMPLE",
+        "quoteSharingAllowed": false
+      },
+      "searchInfo": {
+        "textSnippet": "With over 30 stickers just for fun, this paperback book is a must-have for kids who are interested in the world around them."
+      }
+    }]
+}
+
+export default fetchBooksMockData
