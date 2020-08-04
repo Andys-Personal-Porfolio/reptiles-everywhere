32b31746b5aa85bb33ed5cfdd4300d6abd935720
Andy Tom <andrew.tom@colorado.edu>
2020-08-03 18:20:17 -0600

    Updates

---
+++ precache-manifest.e98ad341becec5360ae7c3ca8a572d79.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b6612482ccba07c4ff1c75ca6fd91d",
+    "url": "/reptile/SummaryView/index.html"
+  },
+  {
+    "revision": "3683335748d9b96d0601",
+    "url": "/reptile/SummaryView/static/css/main.3d948e5a.chunk.css"
+  },
+  {
+    "revision": "12608b1ba7481425301d",
+    "url": "/reptile/SummaryView/static/js/2.87b7286e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reptile/SummaryView/static/js/2.87b7286e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3683335748d9b96d0601",
+    "url": "/reptile/SummaryView/static/js/main.00c0725d.chunk.js"
+  },
+  {
+    "revision": "9eb90be3a76448efef0b",
+    "url": "/reptile/SummaryView/static/js/runtime-main.53cac60a.js"
+  },
+  {
+    "revision": "b993ed369a622e019fbb792d987c10f6",
+    "url": "/reptile/SummaryView/static/media/crocodile-src.b993ed36.png"
+  },
+  {
+    "revision": "e5ff92d7e4defe426ce2fe31f23599d3",
+    "url": "/reptile/SummaryView/static/media/lizard-src.e5ff92d7.png"
+  },
+  {
+    "revision": "25aa1c47d29f34aaf34a60e8158721dc",
+    "url": "/reptile/SummaryView/static/media/reptile-src.25aa1c47.png"
+  },
+  {
+    "revision": "be32f644b85734a5da058282cd4d220d",
+    "url": "/reptile/SummaryView/static/media/snake-src.be32f644.png"
+  },
+  {
+    "revision": "d2d1c27869bdb9f53eea48987c6057ca",
+    "url": "/reptile/SummaryView/static/media/turtle-src.d2d1c278.png"
+  }
+]);
